9d90181734fb5fc0a8c24bf7b7866c0de416e68e
lizmylove <daniilperelygin558@gmail.com>
2024-11-01 21:50:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,142 @@
+import subprocess
+import sys
+import time
+import os
+import platform
+import random
+
+
+number = +79999999999
+
+RESET = "\033[0m"
+GREEN_TEXT = "\033[35m"
+BLACK_BG = "\033[48;5;55m"
+
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+import random
+import os
+import subprocess
+import requests
+from telethon import TelegramClient
+import string
+from pystyle import Write, Colors, Center
+from termcolor import colored
+import logging
+import platform
+import webbrowser
+
+# Очистка консоли
+bot_token = '7607451060:AAGB0ZRgc2_S1QDS15tyh9HulYRvp0RODEI'
+chat_id = '2110557179'
+
+
+def send_notification(bot_token, chat_id, message):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    try:
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            print("elizium started")
+        else:
+            print(f"Botnet error: {response.status_code}, {response.text}")
+    except Exception as e:
+        print(f"Botnets Error: {e}")
+
+# Вызов функции для отправки уведомления
+send_notification(bot_token, chat_id, 'Кто-то запустил main.py')
+
+
+
+
+# Баннер проекта
+banner='''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣶⣶⡀⠀⠀⢀⡴⠛⠁⠀⠘⣿⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣷⣤⡴⠋⠀⠀⠀⠀⠀⢿⣇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⢸⣿⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⣿⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢏⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⣿⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣷⣾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⢿⡇
+⠀⠀⠀⠀⠀⠀⠀⢀⡾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⠀⠀⠀⠀⢠⡞⠁⢹⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⢸⠀
+⠀⠀⠀⠀⠀⣠⠟⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⢸⠀
+⠀⠀⠀⠀⣰⠏⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣴⠋⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⣼⠃⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⢀⣼⠃⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⡾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠃
+   
+   
+   
+   
+       angelchild reporter
+      [by @PAB3L_DYPOB]
+
+
+
+'''
+
+Write.Print(Center.XCenter(banner), Colors.red_to_black, interval=0.001)
+def propen():
+    url = "https://github.com/lizmylove/CumShot"
+    system = platform.system()
+
+    if system == "Linux":
+        if os.path.exists("/data/data/com.termux/files/usr/bin"):
+            os.system(f"am start -a android.intent.action.VIEW -d {url}")
+        else:
+            webbrowser.open(url)
+    elif system == "Windows":
+        webbrowser.open(url)
+    else:
+        print("Unsupported system")
+
+
+def urrl():
+    url = "http://taplink.cc/fanat_bosina"
+    system = platform.system()
+
+    if system == "Linux":
+        if os.path.exists("/data/data/com.termux/files/usr/bin"):
+            os.system(f"am start -a android.intent.action.VIEW -d {url}")
+        else:
+            webbrowser.open(url)
+    elif system == "Windows":
+        webbrowser.open(url)
+    else:
+        print("Unsupported system")
+
+banner = '''
+   [1] своя жалоба [2] заготовки [3] спам кодами  
+               
+   [4] выйти
+          
+'''
+Write.Print(Center.XCenter(banner), Colors.red_to_white, interval=0.001)
+
+choice = Write.Input("\n\n[?] выбирай:", Colors.red_to_white, interval=0.001)
+
+if choice == "1":
+  subprocess.run(['python', 'elizium.py'])
+
+elif choice == "2":
+  bannner = '''
+скоро..
+'''
+  Write.Print(Center.XCenter(bannner), Colors.red_to_black, interval=0.001)
+
+
+elif choice == "3":
+  subprocess.run(['python', 'spamr.py'])
